5d5177e4b4923b85bae03089c1f3c6269c1f7730
MitchelBroekate <mitchelb11@outlook.com>
2023-05-16 13:57:00 +0200

    scripts movement
    movement finished

--- Robbin' Good/Assets/Mitchel/scripts/Movement.cs
+++ Robbin' Good/Assets/Mitchel/scripts/Movement.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed;
-
     public Transform orientation;
 
-     public float hor;
+    [Header("Ground check")]
+    public float playerHeight;
+    public LayerMask WhatIsGround;
+    bool grounded;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+
+    public float hor;
     public float vert;
+
     Vector3 moveDirection;
 
     Rigidbody rb;
@@ -20,13 +28,46 @@ public class Movement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
+    void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, WhatIsGround);
+
+        if(grounded)
+        {
+            rb.drag = groundDrag;
+        } else
+        {
+            rb.drag = 0;
+        }
+
+        MyInput();
+        SpeedControl();
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
     private void MyInput()
     {
         hor = Input.GetAxisRaw("Horizontal");
         vert = Input.GetAxisRaw("Vertical");
     }
-    void Update()
-    {
 
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * vert + orientation.right * hor;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 5f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
