d436f9e3e420b5f2bca9aff44e942b0015e8c2e1
MitchelBroekate <mitchelb11@outlook.com>
2023-05-22 17:00:38 +0200

    Raycast
    raycast for interactions

---
+++ Robbin' Good/Assets/Mitchel/scripts/Interaction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intercation : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.forward, )) ;
+    }
+}
