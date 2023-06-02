809609ada86db7b8848f9b05393df5fdb3b05a43
MitchelBroekate <mitchelb11@outlook.com>
2023-06-02 13:51:22 +0200

    Reload added for ze gun

--- Robbin' Good/Assets/Mitchel/scripts/DamageToEnemy.cs
+++ Robbin' Good/Assets/Mitchel/scripts/DamageToEnemy.cs
@@ -8,16 +8,41 @@ public class DamageToEnemy : MonoBehaviour
 {
     RaycastHit gunHit;
     public GameObject gun;
+    public int maxAmmo = 8;
+    private int currentAmmo;
+    public float reloadTime = 2.5f;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
 
     void Update()
     {
+        if (currentAmmo <= 0) 
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Physics.Raycast(transform.position, transform.forward, out gunHit, 100))
         {
-            if (Input.GetMouseButtonDown(0) && gunHit.transform.gameObject.tag == "Enemy" && gun.activeInHierarchy == true)
+            if (Input.GetMouseButtonDown(0))
             {
-               Health health = gunHit.transform.gameObject.GetComponent<Health>();
-                health.DealDamage(20);
+                currentAmmo --;
+                if (gunHit.transform.gameObject.tag == "Enemy" && gun.activeInHierarchy == true)
+                {
+                    Health health = gunHit.transform.gameObject.GetComponent<Health>();
+                    health.DealDamage(20);
+                }
             }
         }
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
     }
 }
