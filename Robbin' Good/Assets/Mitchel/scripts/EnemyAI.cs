0f04e78accd3c7b6ddffc75c3852d41faac54cd0
MitchelBroekate <mitchelb11@outlook.com>
2023-06-05 16:59:04 +0200

    StateHandeler + other ai
    Vision and patrol done
    
    Statehandeler basics are done

--- Robbin' Good/Assets/Mitchel/scripts/EnemyAI.cs
+++ Robbin' Good/Assets/Mitchel/scripts/EnemyAI.cs
@@ -1,14 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public float viewRadius;
+    public float viewAngle;
 
+    public LayerMask targetPlayer;
+    public LayerMask obstacles;
 
-    private void Update()
+    public GameObject player;
+
+    public enum GuardStates
     {
+        Patrol,
+        Caution,
+        Seen,
+        Alert
+    }
 
+    public GuardStates currentState;
+
+    void Start()
+    {
+        currentState = GuardStates.Patrol;
+
+    }
+
+    void Update()
+    {
+       
+
+        Vector3 playerTarget = player.transform.position - transform.position;
+
+        if (Vector3.Angle(transform.forward, playerTarget) < viewAngle / 2 )
+        {
+            float distanceToTarget = Vector3.Distance(transform.position, player.transform.position);
+            if (distanceToTarget < viewRadius)
+            {
+                if(Physics.Raycast(transform.position, playerTarget, distanceToTarget, obstacles) == false)
+                {
+                    Debug.Log("Holy shieee");
+                }
+            }
+        }
+    }
+
+    public void SetCheckGuardState(GuardStates newEnum)
+    {
+        Enum updatedGuardState = newEnum;
+
+        switch (updatedGuardState)
+        {
+            case GuardStates.Patrol:
+                
+                break;
+        }
     }
 }
