d66edbf66624cb53c05436ed8c379e75d29fb954
MitchelBroekate <mitchelb11@outlook.com>
2023-06-06 13:58:08 +0200

    AI progressie
    AI detecteerd en wacht met spotten

--- Robbin' Good/Assets/Mitchel/scripts/EnemyAI.cs
+++ Robbin' Good/Assets/Mitchel/scripts/EnemyAI.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    #region Variables
+    [Header("Vision")]
     public float viewRadius;
     public float viewAngle;
 
@@ -15,6 +18,17 @@ public class EnemyAI : MonoBehaviour
 
     public GameObject player;
 
+    private IEnumerator coroutine;
+
+    public GuardStates currentState;
+
+    [Header("Patrol")]
+    public Transform[] checkPoints;
+    public int destination = 0;
+    NavMeshAgent agent;
+    #endregion
+
+    #region enum
     public enum GuardStates
     {
         Patrol,
@@ -22,9 +36,9 @@ public class EnemyAI : MonoBehaviour
         Seen,
         Alert
     }
+    #endregion
 
-    public GuardStates currentState;
-
+    #region "Update and start"
     void Start()
     {
         currentState = GuardStates.Patrol;
@@ -44,12 +58,23 @@ public class EnemyAI : MonoBehaviour
             {
                 if(Physics.Raycast(transform.position, playerTarget, distanceToTarget, obstacles) == false)
                 {
+                    StopAllCoroutines();
+                    coroutine = Detection(3);
+                    StartCoroutine(coroutine);
                     Debug.Log("Holy shieee");
+                    Vector3 playerDetection = player.transform.position;
                 }
             }
-        }
-    }
+            if (distanceToTarget > viewRadius)
+            {
 
+            }
+        }
+
+    }
+    #endregion
+
+    #region Switch
     public void SetCheckGuardState(GuardStates newEnum)
     {
         Enum updatedGuardState = newEnum;
@@ -59,6 +84,54 @@ public class EnemyAI : MonoBehaviour
             case GuardStates.Patrol:
                 
                 break;
+
+            case GuardStates.Caution:
+
+                break;
+
+            case GuardStates.Seen:
+
+                break;
+
+            case GuardStates.Alert: 
+
+                break;
+
+            default:
+                Debug.Log("No State Linked");
+                break;
         }
     }
+    #endregion
+
+    #region IEnumerator
+    private IEnumerator Detection(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            ChangeStateAlert();
+        }
+    }
+    #endregion
+
+    #region ChangeStates
+    void ChangeStatePatrol()
+    {
+
+    }
+    public void ChangeStateCaution()
+    {
+
+    }
+    public void ChangeStateSeen()
+    {
+
+    }
+    public void ChangeStateAlert()
+    {
+
+    }
+    #endregion
 }
